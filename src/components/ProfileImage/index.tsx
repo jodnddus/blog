981ac5adc0e6e20a34b107b3ecf4e6f3c420cc8a
jodnddus <lockhost23@gmail.com>
2021-12-20 14:56:31 +0900

    프로필 클릭하면 새 탭에서 나오게 수정

--- src/components/ProfileImage/index.tsx
+++ src/components/ProfileImage/index.tsx
@@ -5,7 +5,7 @@ import { imageCropper, rounded } from "./styles.module.css";
 const ProfileImage = () => {
   return (
     <div className={imageCropper}>
-      <Link to="https://github.com/jodnddus">
+      <Link to="https://github.com/jodnddus" target="_blank">
         <img
           src="https://avatars.githubusercontent.com/u/26714488?s=400&u=c611104c99aabcd132b933b7a49201acedbb8ff5&v=4"
           className={rounded}
